1ac77f46dbba22162acd0d027859acb1131f7cc2
Ilya <karas.ilya@Ukr.net>
2023-07-13 16:56:54 +0200

    Completed task with concatinating

---
+++ Lesson1/Concatinating.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter your first list");
+string firslstring = Console.ReadLine();
+Console.WriteLine("Enter your second list");
+string secondstring = Console.ReadLine();
+if (firslstring.Length == secondstring.Length)
+{
+    Console.WriteLine("c#");
+}
+
+else if (firslstring.Length > secondstring.Length)
+{
+    Console.WriteLine(firslstring + secondstring);
+}
+else
+{
+    char split = secondstring[0];
+    var result = firslstring.Split(split);
+    foreach (var i in result)
+    {
+        Console.WriteLine(i);
+    }
+}
